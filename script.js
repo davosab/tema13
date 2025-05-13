3091c73963814e403d9229f0b9ef9d3bedb1bbb4
davosab <davidsabau72@gmail.com>
2025-05-13 19:35:38 +0300

    drew board

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let canvasWidth = 500;
+let canvasHeight = canvasWidth;
+let s = canvasWidth / 8;
+
+
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(canvasWidth, canvasHeight);
+  initBoard();
+  textAlign(CENTER, CENTER);
+  textSize(s - 20);
+}
+
+function draw() {
+  background("gray");
+  drawSquares();
+  drawPieces();
+}
+
+function initBoard() {
+  board = [
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", ""],
+  ];
+}
+
+function drawSquares() {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((i + j) % 2 == 0) {
+        fill(0);
+      } else {
+        fill(255);
+      }
+      rect(j * s, i * s, s, s);
+    }
+  }
 }
